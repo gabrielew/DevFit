a38bb7889bb80cfd5aa9cc67d76133420987f350
gabrielew <gabrielalcantaratp@gmail.com>
2019-12-13 16:26:06 -0300

    Stacks

---
+++ src/navigators/MainStack.js
@@ -0,0 +1,22 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Preload from '../screens/Preload';
+import StarterStack from './StaterStack';
+// import AppTab from './AppTab';
+
+const MainStack = createStackNavigator(
+  {
+    Preload,
+    StarterStack,
+    // AppTab,
+  },
+  {
+    initialRouteName: 'Preload',
+    defaultNavigationOptions: {
+      header: null,
+    },
+  },
+);
+
+export default createAppContainer(MainStack);
---
+++ src/navigators/StaterStack.js
@@ -0,0 +1,11 @@
+import {createStackNavigator} from 'react-navigation-stack';
+
+import StarterIntro from '../screens/StarterIntro';
+import StarterName from '../screens/StarterName';
+import StarterDays from '../screens/StarterDays';
+
+export default createStackNavigator({
+  StarterIntro,
+  StarterName,
+  StarterDays,
+});
