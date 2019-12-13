efb35e593267ea69058b11eca1b04d8e173f5506
gabrielew <gabrielalcantaratp@gmail.com>
2019-12-13 17:15:42 -0300

    stacks

--- src/navigators/StarterStack.js
+++ src/navigators/StarterStack.js
@@ -3,9 +3,13 @@ import {createStackNavigator} from 'react-navigation-stack';
 import StarterIntro from '../screens/StarterIntro';
 import StarterName from '../screens/StarterName';
 import StarterDays from '../screens/StarterDays';
+import StarterLevel from '../screens/StarterLevel';
+import StarterRecommendations from '../screens/StarterRecommendations';
 
 export default createStackNavigator({
   StarterIntro,
   StarterName,
   StarterDays,
+  StarterLevel,
+  StarterRecommendations,
 });
