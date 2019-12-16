8c32fdd47f286a7e6779101308d3f808505c0919
gabrielew <gabrielalcantaratp@gmail.com>
2019-12-16 10:55:26 -0300

    BottomTab Navigator

---
+++ src/navigators/AppTab.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import CustomTabBar from '../components/CustomTabBar';
+import HomeStack from './HomeStack';
+// import WorkoutStack from './WorkoutStack';
+// import MyWorkoutsStack from './MyWorkoutsStack';
+
+export default createBottomTabNavigator(
+  {
+    HomeStack,
+    // WorkoutStack,
+    // MyWorkoutsStack,
+  },
+  {
+    tabBarComponent: props => (
+      <CustomTabBar
+        {...props}
+        items={[
+          {
+            type: 'regular',
+            text: 'Home',
+            icon: require('../assets/home.png'),
+            route: 'HomeStack',
+          },
+          {
+            type: 'big',
+            icon: require('../assets/dumbbell.png'),
+            route: 'WorkoutStack',
+          },
+          {
+            type: 'regular',
+            text: 'My Workouts',
+            icon: require('../assets/myworkouts.png'),
+            route: 'MyWorkouts',
+          },
+        ]}
+      />
+    ),
+  },
+);
