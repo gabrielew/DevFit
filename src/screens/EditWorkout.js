56054db199aec2ba464bb5b6ff5204676da89b22
gabrielew <gabrielalcantaratp@gmail.com>
2019-12-19 08:32:03 -0300

    Edit workout

---
+++ src/screens/EditWorkout.js
@@ -0,0 +1,217 @@
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
+import {connect} from 'react-redux';
+import DefaultButton from '../components/DefaultButton';
+import ExerciseItemEdit from '../components/ExerciseItemEdit';
+import CustomModal from '../components/CustomModal';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  margin: 20px;
+`;
+
+const NameInput = styled.TextInput`
+  border: 1px solid #7159c1;
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  font-size: 16px;
+  padding: 10px;
+`;
+
+const ExercisesArea = styled.View`
+  flex: 1;
+  margin-top: 20px;
+  padding-top: 20px;
+  border-top-width: 1px;
+  border-top-color: #ccc;
+`;
+
+const ButtonText = styled.Text``;
+
+const ExercisesList = styled.FlatList`
+  flex: 1;
+  padding-top: 20px;
+`;
+
+const ModalLabel = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+  margin-top: 10px;
+`;
+
+const ModalMuscles = styled.ScrollView``;
+
+const ModalInput = styled.TextInput`
+  width: 100%;
+  font-size: 14px;
+  color: #333;
+  height: 40px;
+  border-bottom-width: 1px;
+  border-bottom-color: #ccc;
+`;
+
+const ModalMuscle = styled.TouchableOpacity`
+  width: 50px;
+  height: 50px;
+  background-color: #eee;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  opacity: ${props => props.opacity};
+`;
+
+const ModalMuscleImage = styled.Image`
+  width: 35px;
+  height: 35px;
+`;
+
+const Page = props => {
+  let workout =
+    props.navigation.state.params && props.navigation.state.params.workout
+      ? props.navigation.state.params.workout
+      : false;
+  const [id, setId] = useState(workout ? workout.id : '');
+  const [name, setName] = useState(workout ? workout.name : '');
+  const [exercises, setExercises] = useState(workout ? workout.exercises : []);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [modalId, setModalId] = useState('');
+  const [modalName, setModalName] = useState('');
+  const [modalMuscle, setModalMuscle] = useState('');
+  const [modalSets, setModalSets] = useState('');
+  const [modalReps, setModalReps] = useState('');
+  const [modalLoad, setModalLoad] = useState('');
+
+  const editExercise = exercise => {
+    setModalId(exercise.id);
+    setModalName(exercise.name);
+    setModalMuscle(exercise.muscle);
+    setModalSets(exercise.sets);
+    setModalReps(exercise.reps);
+    setModalLoad(exercise.load);
+    setModalVisible(true);
+  };
+  const delExercise = exercise => {
+    let newExercises = [...exercises];
+    newExercises = newExercises.filter(i => i.id != exercise.id);
+
+    setExercises(newExercises);
+  };
+
+  return (
+    <Container>
+      <CustomModal
+        visible={modalVisible}
+        closeAction={() => setModalVisible(false)}>
+        <ModalLabel>Focus Muscle</ModalLabel>
+        <ModalMuscles horizontal={true} showHorizontalScrollIndicator={false}>
+          <ModalMuscle opacity={modalMuscle == 'abs' ? 1 : 0.3}>
+            <ModalMuscleImage source={require(`../assets/muscles/abs.png`)} />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'back' ? 1 : 0.3}>
+            <ModalMuscleImage source={require(`../assets/muscles/back.png`)} />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'biceps' ? 1 : 0.3}>
+            <ModalMuscleImage
+              source={require(`../assets/muscles/biceps.png`)}
+            />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'chest' ? 1 : 0.3}>
+            <ModalMuscleImage source={require(`../assets/muscles/chest.png`)} />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'gluteos' ? 1 : 0.3}>
+            <ModalMuscleImage
+              source={require(`../assets/muscles/gluteos.png`)}
+            />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'legs' ? 1 : 0.3}>
+            <ModalMuscleImage source={require(`../assets/muscles/legs.png`)} />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'shoulders' ? 1 : 0.3}>
+            <ModalMuscleImage
+              source={require(`../assets/muscles/shoulders.png`)}
+            />
+          </ModalMuscle>
+          <ModalMuscle opacity={modalMuscle == 'triceps' ? 1 : 0.3}>
+            <ModalMuscleImage
+              source={require(`../assets/muscles/triceps.png`)}
+            />
+          </ModalMuscle>
+        </ModalMuscles>
+        <ModalLabel>Exercise</ModalLabel>
+        <ModalInput
+          value={modalName}
+          onChangeText={name => setModalName(name)}
+        />
+      </CustomModal>
+
+      <NameInput
+        value={name}
+        onChangeText={name => setName(name)}
+        placeholder="Type workout name"
+      />
+      <ExercisesArea>
+        <DefaultButton bgColor="">
+          <ButtonText>Add Exercise</ButtonText>
+        </DefaultButton>
+        <ExercisesList
+          data={exercises}
+          renderItem={({item}) => (
+            <ExerciseItemEdit
+              data={item}
+              editAction={() => editExercise(item)}
+              delAction={() => delExercise(item)}
+            />
+          )}
+          keyExtractor={item => item.name}
+        />
+      </ExercisesArea>
+    </Container>
+  );
+};
+
+Page.navigationOptions = ({navigation}) => {
+  let isEdit =
+    navigation.state.params && navigation.state.params.workout ? true : false;
+
+  const SaveArea = styled.TouchableOpacity`
+    width: 30px;
+    height: 30px;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const SaveImage = styled.Image`
+    width: 25px;
+    height: 25px;
+  `;
+
+  const SaveWorkoutButton = () => {
+    return (
+      <SaveArea>
+        <SaveImage source={require('../assets/check-black.png')} />
+      </SaveArea>
+    );
+  };
+
+  return {
+    title: isEdit ? 'Edit Workout' : 'Add Workout',
+    headerRight: <SaveWorkoutButton />,
+    headerRightContainerStyle: {
+      marginRight: 10,
+    },
+  };
+};
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
